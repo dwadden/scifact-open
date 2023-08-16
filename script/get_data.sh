301ed73c042f746afd48b89cdb6dcdedc881089b
David Wadden <davidw@ip-192-168-0-13.us-west-2.compute.internal>
2023-08-16 00:04:48 -0700

    Get data from AWS rather than google drive.

--- script/get_data.sh
+++ script/get_data.sh
@@ -1,8 +1,5 @@
 # Download and decompress data.
 
-gdown https://drive.google.com/uc?id=1sz-9hJTVex8h_jm_NPjXfmTrhi_8Sz-U
+wget wget "https://scifact.s3.us-west-2.amazonaws.com/scifact-open/latest/scifact_open.tar.gz"
 tar -xvf scifact_open.tar.gz
-mv scifact_open/data .
-mv scifact_open/prediction .
-rmdir scifact_open
 rm scifact_open.tar.gz
